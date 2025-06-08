dd8363ff69e9a9d0580b822ac363d802aa94dc5c
Malinde Godwin <malindegodwin9@gmail.com>
2025-06-08 23:11:30 +0300

    changes made

--- script.js
+++ script.js
@@ -172,3 +172,17 @@ allNavLink.addEventListener('click', function (e){
     document.querySelector(id).scrollIntoView({behavior: "smooth"});
   }
 })
+
+// Going downward child
+console.log(h1.querySelectorAll('.highlight'));
+console.log(h1.childNodes)
+console.log(h1.children);
+h1.firstElementChild.style.color = 'White';
+h1.lastElementChild.style.color = 'orangered'
+
+// Going Upwards: parents
+console.log(h1.parentNode);
+console.log(h1.parentElement);
+
+h1.closest('.header').style.background = 'var(--gradient-secondary)';
+h1.closest('h1').style.background = 'var(--gradient-primary)';
