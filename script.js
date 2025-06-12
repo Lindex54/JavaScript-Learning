4cc60ce2a252eb5e0c4b3e4939893b23b7422d20
Malinde Godwin <malindegodwin9@gmail.com>
2025-06-12 14:34:55 +0300

    Changes

--- script.js
+++ script.js
@@ -275,7 +275,34 @@ const sectionObserver = new IntersectionObserver(revealSections, {
 allSectons.forEach(function(section){
   sectionObserver.observe(section);
   section.classList.add('section--hidden');
-})
+});
+
+// Lazy loading images
+const imgTargets = document.querySelectorAll('img[data-src]');
+
+const loadImg = function(entries, observer){
+  const [entry] = entries;
+  if (!entry.isIntersecting) return;
+
+  // Replace src with data-src
+  entry.target.src = entry.target.dataset.src;
+  entry.target.addEventListener('load', function(){
+    entry.target.classList.remove('lazy-img');
+  });
+
+  observer.unobserve(entry.target);
+};
+
+const imgObserver = new IntersectionObserver(loadImg, {
+ root: null,
+ threshold: 0,
+ rootMargin: '200px'
+});
+
+imgTargets.forEach(img => imgObserver.observe(img))
+
+
+
 // another option
 // nav.addEventListener('mouseover', function(e){
 //  handlehover(e, 0.5);
